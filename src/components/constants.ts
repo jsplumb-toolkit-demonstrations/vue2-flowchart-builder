ec961e5434e1873e9d9b2b468e62cc2517a620f3
Simon Porritt <simon@jsplumbtoolkit.com>
2022-10-26 21:14:11 +1100

    extract some constants

---
+++ src/components/constants.ts
@@ -0,0 +1,16 @@
+
+export const DIALOG_TEXT = "dlgText"
+export const DIALOG_CONFIRM = "dlgConfirm"
+export const DIALOG_MESSAGE = "dlgMessage"
+
+export const NODE_TYPE_START = "start"
+export const NODE_TYPE_SELECTABLE = "selectable"
+export const NODE_TYPE_QUESTION = "question"
+export const NODE_TYPE_ACTION = "action"
+export const NODE_TYPE_OUTPUT = "output"
+
+export const EDGE_TYPE_RESPONSE = "response"
+
+export const PORT_TYPE_START = "start"
+export const PORT_TYPE_SOURCE = "source"
+export const PORT_TYPE_TARGET = "target"
