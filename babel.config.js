50d4134a03918c856c56d1b5d0b05695749a9d37
Simon Porritt <simon@jsplumbtoolkit.com>
2022-10-26 21:24:02 +1100

    add various plugins, related to the vue-component-class lib and its use of decorators

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,12 @@
 module.exports = {
-  presets: [
-    '@vue/app'
+  "presets": [
+    '@vue/cli-plugin-babel/preset'
+  ],
+  "plugins": [
+    ["@babel/proposal-decorators", { "legacy": true }],
+    ["@babel/proposal-class-properties", { "loose": true }],
+    ["@babel/plugin-proposal-private-methods", { "loose": true }],
+    ["@babel/plugin-proposal-private-property-in-object", { "loose": true }]
+
   ]
 }
